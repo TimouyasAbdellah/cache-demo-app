b2cd166729149539ff7714cecf3c87fa393cc129
abdellah.timouyas <abdellah.timouyas@gmail.com>
2018-11-25 20:33:58 +0100

    Add webpack config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {VersionCheckService} from './version-check.service';
+import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'cache-demo-app';
+
+
+  constructor(private versionCheckService: VersionCheckService) {
+  }
+
+  ngOnInit(): void {
+    this.versionCheckService.initVersionCheck(environment.versionCheckURL);
+  }
+
+
 }
